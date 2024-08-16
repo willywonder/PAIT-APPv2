1cd5bb9416103b3e8aa60f5d0fda9435d21a94fa
EssKare <esskaree@gmail.com>
2024-08-16 16:02:24 +0300

    updates

---
+++ src/app/Layout/Homelayout.jsx
@@ -0,0 +1,22 @@
+import Dock from "@/Components/Dock/dock";
+import React from "react";
+
+export default function Homelayout({ children }) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+       
+      }}
+    >
+      <main>{children}</main>
+      <footer>
+        <Dock />
+      </footer>
+    </div>
+  );
+}
---
+++ src/app/loading.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Image from "next/image";
+export default function loading() {
+  return <Image src="/Group.svg" alt="image" width="257" height="71" />;
+}
