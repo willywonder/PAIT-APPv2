bbf21b0bfe87bffb4dd5d95b9c799a3839c50c43
willy wonder <willywanjuki12@gmail.com>
2024-09-10 17:55:56 +0300

    home landing page fixed

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,21 @@ import { useState, useEffect } from "react";
 import Homelayout from "./Layout/homelayout";
 import { useRouter } from "next/navigation";
 
+const coins = [
+  {
+    name: "Solana",
+    price: "USD $140.90",
+    symbol: "SOL",
+    logo: "/logo3.png",
+  },
+  {
+    name: "Toncoin",
+    price: "USD $7.50",
+    symbol: "Ton",
+    logo: "/image 1.png",
+  },
+];
+
 const Home = () => {
   const [showSplash, setShowSplash] = useState(true);
   const router = useRouter();
@@ -32,78 +47,50 @@ const Home = () => {
         <div className="home-logo">
           <Image src="/Group.svg" alt="image" width="121" height="33" />
         </div>
-        <div className="pait">
-          <div className="wordings">
-            <h1>Buy PAiT</h1>
-            <p>Unlock profits exclusive to PAiT holders.</p>
-            <div className="one">
-              {" "}
-              <span>USD $0.50</span> / 1 $PAIT
-            </div>
-            <button className="buybutton">
-              <div className="Buyy-btn">
-                <span>Buy Now </span>
-                <Image
-                  src="/blackright.png"
-                  alt="image"
-                  width="16"
-                  height="17"
-                />
+        <div className="coins-container">
+          <div className="pait">
+            <div className="wordings">
+              <h2>Buy PAiT</h2>
+              <p>Unlock profits exclusive to PAiT holders.</p>
+              <div className="one">
+                <span>USD $0.50</span> / 1 $PAIT
               </div>
-            </button>
-          </div>
-          <div className="logo">
-            <Image src="/Logo.png" alt="image" width="88" height="88" />
-          </div>
-        </div>
-        <div className="other">Other Tokens</div>
-        <div className="solona">
-          <div className="logo3">
-            <Image src="/logo3.png" alt="image" width="56" height="56" />
-          </div>
-          <div className="all">
-            <div className="sol">Solana</div>
-            <div className="two">
-              <span>USD $140.90</span> / 1 $SOL
+              <button className="buybutton">Buy Now &gt;</button>
+            </div>
+            <div className="logo">
+              <Image src="/Logo.png" alt="image" width="88" height="88" />
             </div>
           </div>
-          <div className="sol-btn">
-            <button type="submit" className="buttton">
-              <div className="Inside-btn">
-                <span>Buy</span>
+          <div className="coins-header">Other Tokens</div>
+          {coins.map((coin, index) => (
+            <div className="coinscard" key={index}>
+              <div className="wrapper">
                 <Image
-                  src="/chevronright.png"
-                  alt="image"
-                  width="16"
-                  height="17"
+                  src={coin.logo}
+                  alt={`${coin.name} logo`}
+                  width="56"
+                  height="56"
                 />
+                <div className="all">
+                  <div className="name">{coin.name}</div>
+                  <div className="two">
+                    <span>{coin.price}</span> / 1 ${coin.symbol}
+                  </div>
+                </div>
               </div>
-            </button>
-          </div>
-        </div>
-        <div className="toncoin">
-          <div className="logo3">
-            <Image src="/image 1.png" alt="image" width="56" height="56" />
-          </div>
-          <div className="all2">
-            <div className="ton">Toncoin</div>
-            <div className="three">
-              <span>USD $7.50</span> / 1 $Ton
+              <div className="buy-btn">
+                <button type="submit" className="inside-btn">
+                  <span>Buy</span>
+                  <Image
+                    src="/chevronright.png"
+                    alt="buy chevron"
+                    width="16"
+                    height="17"
+                  />
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="ton-btn">
-            <button type="submit" className="buttton">
-              <div className="Inside-btn">
-                <span>Buy</span>
-                <Image
-                  src="/chevronright.png"
-                  alt="image"
-                  width="16"
-                  height="17"
-                />
-              </div>
-            </button>
-          </div>
+          ))}
         </div>
         <div className="button-container">
           <button className="walletbtn" onClick={handleConnectWallet}>
