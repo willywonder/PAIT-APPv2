213480317452ccacbf24deca066b1a58e56415c7
willy wonder <willywanjuki12@gmail.com>
2024-09-12 16:47:00 +0300

    Home landing page completed,walletcreation1(page2)completed

--- src/Components/Walletcreation1/page.js
+++ src/Components/Walletcreation1/page.js
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import "./walletcreate1.css";
-import * as ethers from 'ethers';
+import * as ethers from "ethers";
 
 const Walletcreation1 = ({ onClose }) => {
   const router = useRouter();
@@ -13,9 +13,9 @@ const Walletcreation1 = ({ onClose }) => {
   };
 
   const handleLinkExistingWallet = async () => {
-    if (typeof window.ethereum !== 'undefined') {
+    if (typeof window.ethereum !== "undefined") {
       try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        await window.ethereum.request({ method: "eth_requestAccounts" });
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const userAddress = await signer.getAddress();
@@ -25,7 +25,7 @@ const Walletcreation1 = ({ onClose }) => {
         console.error("Failed to connect wallet", err);
       }
     } else {
-      console.log('MetaMask is not installed!');
+      console.log("MetaMask is not installed!");
     }
   };
 
@@ -39,26 +39,35 @@ const Walletcreation1 = ({ onClose }) => {
           <Image src="/circlex.png" alt="image" width="24" height="24" />
         </div>
       </div>
-      <div className="connect">Connect Wallet</div>
-      <div className="hand">
-        <Image src="/hand.png" alt="image" width="256" height="212" />
-      </div>
-      <div className="text1">
-        Your web3 wallet acts as your user account, and is where you store
-        your assets like coins.
-      </div>
-      <button className="createbtn" onClick={handleCreateNewWallet}>
-        <div className="Inside-createbtn">
-          <Image src="/plus.png" alt="image" width="24" height="24" />
-          <span>Create New Wallet</span>
+
+      <div className="content-wrapper">
+        <div className="connect">Connect Wallet</div>
+        <div className="hand">
+          <Image src="/hand.png" alt="image" width="256" height="212" />
         </div>
-      </button>
-      <button type="submit" className="linkbtn" onClick={handleLinkExistingWallet}>
-        <div className="Inside-linkbtn">
-          <Image src="/link2.png" alt="image" width="24" height="24" />
-          <span>Link Existing Wallet</span>
+        <div className="text1">
+          Your web3 wallet acts as your user account, and
+          <br /> is where you store your assets like coins.
         </div>
-      </button>
+      </div>
+      <div className="buttons-wrapper">
+        <button className="createbtn" onClick={handleCreateNewWallet}>
+          <div className="Inside-createbtn">
+            <Image src="/plus.png" alt="image" width="24" height="24" />
+            <span>Create New Wallet</span>
+          </div>
+        </button>
+        <button
+          type="submit"
+          className="linkbtn"
+          onClick={handleLinkExistingWallet}
+        >
+          <div className="Inside-linkbtn">
+            <Image src="/link2.png" alt="image" width="24" height="24" />
+            <span>Link Existing Wallet</span>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
